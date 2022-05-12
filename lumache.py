59147ec8c61bb5b88394335f42f1547eadd634d6
boardcon <105197449+boardcon@users.noreply.github.com>
2022-05-12 15:12:48 +0800

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -1,5 +1,5 @@
 """
-Lumache - Python library for cooks and food lovers.
+Boardcon Embedded Design -  an Original Design Manufacturer dedicated to providing high quality ARM core based SBC and SoM.
 """
 
 __version__ = "0.1.0"
