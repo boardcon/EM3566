b283ec2c9be3bc254b74f40ca33afe85de523e26
Woo <105197449+boardcon@users.noreply.github.com>
2022-06-02 19:31:35 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,6 @@ intersphinx_disabled_domains = ['std']
 
 # -- Options for HTML output
 
-html_theme = "jekyll-theme-modernist"
-
-
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
 
